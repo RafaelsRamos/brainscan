fa7efa0e27a8eec983486a1a9b073b8b605f01b0
Rafael Ramos <rafael.ramos@farfetch.com>
2022-11-18 18:45:36 +0000

    Add resources

--- app/src/main/java/com/scookie/brainscanner/common/theme/Type.kt
+++ app/src/main/java/com/scookie/brainscanner/common/theme/Type.kt
@@ -2,27 +2,26 @@ package com.scookie.brainscanner.common.theme
 
 import androidx.compose.material.Typography
 import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 
-// Set of Material typography styles to start with
 val Typography = Typography(
     body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+        color = DefaultTextColor,
+        fontFamily = Fonts.family,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
         fontSize = 14.sp
     ),
-    caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 12.sp
+    h1 = TextStyle(
+        color = DefaultTextColor,
+        fontFamily = Fonts.family,
+        fontWeight = FontWeight.Bold,
+        fontSize = 22.sp
+    ),
+    h2 = TextStyle(
+        color = DefaultTextColor,
+        fontFamily = Fonts.family,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp
     )
-    */
 )
