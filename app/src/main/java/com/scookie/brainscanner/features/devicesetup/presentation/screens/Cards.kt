77fa553ca7177f835dae48889635c0b109691fff
Rafael Ramos <rafael.ramos@farfetch.com>
2022-11-21 20:16:29 +0000

    Adjust graphs

--- app/src/main/java/com/scookie/brainscanner/features/devicesetup/presentation/screens/Cards.kt
+++ app/src/main/java/com/scookie/brainscanner/features/devicesetup/presentation/screens/Cards.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -22,7 +21,7 @@ import androidx.compose.ui.unit.sp
 import com.scookie.brainscanner.common.theme.ButtonBlue
 import com.scookie.brainscanner.common.theme.TextWhite
 import com.scookie.brainscanner.features.devicesetup.presentation.models.Feature
-import com.scookie.brainscanner.utils.standardQuadFromTo
+import com.scookie.brainscanner.utils.drawPathWith
 
 @Composable
 fun FeatureItem(
@@ -39,22 +38,13 @@ fun FeatureItem(
         val height = constraints.maxHeight
 
         // Medium colored path
-        val mediumColoredPoint1 = Offset(0f, height * 0.3f)
-        val mediumColoredPoint2 = Offset(width * 0.1f, height * 0.35f)
-        val mediumColoredPoint3 = Offset(width * 0.4f, height * 0.05f)
-        val mediumColoredPoint4 = Offset(width * 0.75f, height * 0.7f)
-        val mediumColoredPoint5 = Offset(width * 1.4f, -height.toFloat())
-
-        val mediumColoredPath = Path().apply {
-            moveTo(mediumColoredPoint1.x, mediumColoredPoint1.y)
-            standardQuadFromTo(mediumColoredPoint1, mediumColoredPoint2)
-            standardQuadFromTo(mediumColoredPoint2, mediumColoredPoint3)
-            standardQuadFromTo(mediumColoredPoint3, mediumColoredPoint4)
-            standardQuadFromTo(mediumColoredPoint4, mediumColoredPoint5)
-            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
-            lineTo(-100f, height.toFloat() + 100f)
-            close()
-        }
+        val mediumPoint1 = Offset(0f, height * 0.3f)
+        val mediumPoint2 = Offset(width * 0.1f, height * 0.35f)
+        val mediumPoint3 = Offset(width * 0.4f, height * 0.05f)
+        val mediumPoint4 = Offset(width * 0.75f, height * 0.7f)
+        val mediumPoint5 = Offset(width * 1.4f, -height.toFloat())
+        val mediumColorPoints = listOf(mediumPoint1, mediumPoint2, mediumPoint3, mediumPoint4, mediumPoint5)
+        val mediumColoredPath = constraints.drawPathWith(mediumColorPoints)
 
         // Light colored path
         val lightPoint1 = Offset(0f, height * 0.35f)
@@ -62,17 +52,9 @@ fun FeatureItem(
         val lightPoint3 = Offset(width * 0.3f, height * 0.35f)
         val lightPoint4 = Offset(width * 0.65f, height.toFloat())
         val lightPoint5 = Offset(width * 1.4f, -height.toFloat() / 3f)
+        val lightColorPoints = listOf(lightPoint1, lightPoint2, lightPoint3, lightPoint4, lightPoint5)
+        val lightColoredPath = constraints.drawPathWith(lightColorPoints)
 
-        val lightColoredPath = Path().apply {
-            moveTo(lightPoint1.x, lightPoint1.y)
-            standardQuadFromTo(lightPoint1, lightPoint2)
-            standardQuadFromTo(lightPoint2, lightPoint3)
-            standardQuadFromTo(lightPoint3, lightPoint4)
-            standardQuadFromTo(lightPoint4, lightPoint5)
-            lineTo(width.toFloat() + 100f, height.toFloat() + 100f)
-            lineTo(-100f, height.toFloat() + 100f)
-            close()
-        }
         Canvas(
             modifier = Modifier
                 .fillMaxSize()
--- app/src/main/java/com/scookie/brainscanner/utils/PathUtils.kt
+++ app/src/main/java/com/scookie/brainscanner/utils/PathUtils.kt
@@ -2,9 +2,29 @@ package com.scookie.brainscanner.utils
 
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.unit.Constraints
 import kotlin.math.abs
 
-fun Path.standardQuadFromTo(from: Offset, to: Offset) {
+fun Constraints.drawPathWith(points: List<Offset>): Path {
+
+    return Path().apply {
+
+        val firstPoint = points.first()
+        moveTo(firstPoint.x, firstPoint.y)
+
+        for (index in 1 until  points.size){
+            standardQuadFromTo(points[index - 1], points[index])
+        }
+
+        lineTo(maxWidth.toFloat() + 100F, maxHeight + 100F)
+        lineTo(-100f, maxHeight.toFloat() + 100f)
+        close()
+
+    }
+
+}
+
+private fun Path.standardQuadFromTo(from: Offset, to: Offset) {
     quadraticBezierTo(
         from.x,
         from.y,
