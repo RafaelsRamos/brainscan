49d392338a1df92fa1822a2490c00686935440ee
Rafael Ramos <rafael.ramos@farfetch.com>
2022-11-22 09:14:49 +0000

    Update colors

--- app/src/main/java/com/scookie/brainscanner/common/theme/Color.kt
+++ app/src/main/java/com/scookie/brainscanner/common/theme/Color.kt
@@ -8,5 +8,20 @@ val Purple700 = Color(0xFF3700B3)
 val Teal200 = Color(0xFF03DAC5)
 
 val TextWhite = Color(0xffeeeeee)
+val DefaultTextColor = Color(0xff4d4c59)
 
 val ButtonGreen = Color(0xFF2DB591)
+
+val DeepBlue = Color(0xff576dff)
+val ButtonBlue = Color(0xff505cf3)
+val AquaBlue = Color(0xff6b7389)
+
+val AppBackground = Color(0xffF7F6F0)
+
+val MainButtonsColor = Color(0xFFa7cbd5)
+val MainButtonsTextColor = Color(0xFF4d4c59)
+val StopButtonColor = Color(0xFFeb8a8a)
+
+val DividerColor = Color(0x15000000)
+
+val NavigationSelectedBackground = Color(0xAA505cf3)
---
+++ app/src/main/java/com/scookie/brainscanner/common/theme/GraphColors.kt
@@ -0,0 +1,22 @@
+package com.scookie.brainscanner.common.theme
+
+import androidx.compose.ui.graphics.Color
+
+val BlueShadeOne = Color(0xFF3676f5)
+val BlueShadeTwo = Color(0xFF4983f5)
+val BlueShadeThree = Color(0xFF6093f7)
+val BlueShadeFour = Color(0xFF79a2f2)
+val BlueShadeFive = Color(0xFF92b2f0)
+
+val GreenShadeOne = Color(0xFF1f9141)
+val GreenShadeTwo = Color(0xFF359c54)
+val GreenShadeThree = Color(0xFF4ba766)
+val GreenShadeFour = Color(0xFF62b27a)
+val GreenShadeFive = Color(0xFF78bd8d)
+
+// Polarity Palette -> https://www.color-hex.com/color-palette/1019868
+val PolarityDarkOrange = Color(0xFFfc543a)
+val PolarityLightOrange = Color(0xFFff9566)
+val PolarityYellow = Color(0xFFffd966)
+val PolarityGreen = Color(0xFF93c47d)
+val PolarityBlue = Color(0xFF6fa8dc)
